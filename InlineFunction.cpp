665cd1430277982a4d374658f9819f1582707290
Sahil Desai <145180733+sahildesai21@users.noreply.github.com>
2024-05-21 20:49:10 +0530

    Update InlineFunction.cpp

--- InlineFunction.cpp
+++ InlineFunction.cpp
@@ -2,7 +2,10 @@
 instead of calling the the same function repeatdly( whenever we run the code or complie). 
 inline function will replce the normal function code with inline function code in compile time(when run the code once). so, that 
 only once the function will get complied and returns the value wherever function has been called.
-NOTE: the will no change in the ouput.  
+NOTE: 1. there will no change in the ouput.  
+    2. make sure you don't use the inline fuction when there is huge block of code
+       because it will use the chache memory.
+    3. use the inline function for short block of code. 
 */
 #include<iostream>
 using namespace std;
