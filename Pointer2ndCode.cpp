3a1e7dab934884afd5b967a661da16790abe053b
Sahil Desai <145180733+sahildesai21@users.noreply.github.com>
2024-06-15 16:44:23 +0530

    Update Pointer2ndCode.cpp

--- Pointer2ndCode.cpp
+++ Pointer2ndCode.cpp
@@ -4,7 +4,11 @@ if you are asigning the value to the normal varialbe using pointer var.. (ptr=x)
 if you are asigning the value to the pointer varialbe using normal var.. (x=ptr) then synatx will be: variable=*pointer.
 if you are asigning the value just to the pointer varialbe then synatx will be: *pointer=Value(No.).
 if you are asigning the value just to the normal varialbe then synatx will be: varible=Value(No.): like normally asigning the value.
-note: while using/typeing the pointer varialbe always use with the star synbol: *pointer.
+note: 1. While using/typeing the pointer varialbe always use with the star symbol: *pointer.
+
+when to use pointer: 
+1. If pointer arithmetic or passing a NULL pointer is needed. For example, for arrays (Note that accessing an array is implemented using pointer arithmetic).
+2. To implement data structures like a linked list, a tree, etc. and their algorithms. This is so because, in order to point to different cells, we have to use the concept of pointers.  
 
 */
 #include<iostream>
